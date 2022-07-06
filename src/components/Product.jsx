03dccdebdc80204eb93176c3853ea6f133171b62
MuhammadTaqi8 <taqi0413@gmail.com>
2022-07-06 22:46:20 +0500

    added products and Navbar

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Product = ({product}) => {
   return (
     <>
-        <div class="card-body">
+          <div class="card-body">
             <h5 class="card-title">{product.name}</h5>
             <p class="card-text">{product.description}</p>
-        </div>
+            <p>{product.price}</p>
+          </div>
     </>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import Product from './Product'
 
 const products = [
-    {id: 1, name: "shoes", description: "Walking Shoes"},
-    {id: 2, name: "jacket", description: "Winter jackets"},
-
+    {id: 1, name: "shoes", description: "Walking Shoes", price: 100},
+    {id: 2, name: "jacket", description: "Winter jackets", price: 200},
+    {id: 1, name: "shoes", description: "Walking Shoes", price: 100},
+    {id: 2, name: "jacket", description: "Winter jackets", price: 200},
+    
 ]
 
 const Products = () => {
   return (
     <main>
         <div>
-            
-            {products.map((product)=> (
-                <Product  product = {product}/>
-            ))}
+          <h2>Product Library</h2>
+            <div className='row'>
+                {products.map((product)=> (
+                    <div className='col'>
+                        <Product  product = {product}/>
+                    </div>
+                ))}
+            </div>
         </div>
     </main>
 )

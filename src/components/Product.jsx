b1245bd5c23d043bd3c47cd2dc4602541ce6d51a
MuhammadTaqi8 <taqi0413@gmail.com>
2022-07-07 18:55:55 +0500

    SNTCP added button to product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,10 +5,11 @@ const Product = ({product}) => {
   return (
     <>
           <div class="card-body">
-            <img src={product.image} alt="image" width='150px' height='150px' />
+            <img src={product.image} alt="im" width='150px' height='150px' />
             <h5 class="card-title">{product.title}</h5>
             <p class="card-text">{product.description}</p>
             <p><strong>${product.price}</strong></p>
+            <button className='btn btn-primary'>Add to cart</button>
           </div>
     </>
   )
